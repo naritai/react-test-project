d5f4e79d4de59495dfcd0087828273462f4fc0bb
naritai <alexandr.voronetskiy@gmail.com>
2023-06-01 17:40:43 +0300

    fix ButtonSwitcher outer styles

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './ThemeSwitcher.module.scss';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import DarkIcon from 'shared/assets/icons/dark-theme.svg';
 import LightIcon from 'shared/assets/icons/light-theme.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/ui/Button';
+import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps { 
    className?: string; 
@@ -13,12 +13,14 @@ export function ThemeSwitcher({ className }: ThemeSwitcherProps) {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <Button
-      theme={ThemeButton.CLEAR}
-      className={classNames(cls.themeswitcher, {}, [className])}
-      onClick={toggleTheme}
-    >
-      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
-    </Button>
+    <div className={cls.wrapper}>
+      <Button
+        theme={ThemeButton.CLEAR}
+        className={classNames(cls.themeswitcher, {}, [className])}
+        onClick={toggleTheme}
+      >
+        {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+      </Button>
+    </div>
   )
 }
