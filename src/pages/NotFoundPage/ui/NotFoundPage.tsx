8e46112fa29329ecc4767b1b38031ba800d10837
naritai <alexandr.voronetskiy@gmail.com>
2023-06-02 14:24:56 +0300

    not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './NotFoundPage.module.scss'
+
+interface NotFoundPageProps {
+  className?: string
+}
+
+export function NotFoundPage ({ className }: NotFoundPageProps): JSX.Element {
+  return (
+    <div className={classNames(cls.notfoundpage, {}, [className])}>
+      {'Page not found :('}
+    </div>
+  )
+}
